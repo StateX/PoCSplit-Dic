a1ff44e273331c9139e29d4828c2c998ce84ea23
Albert Sanchez Carbonell <statexblog@gmail.com>
2014-11-24 16:36:01 +0100

    Txt Split and catch

---
+++ PocSplit.py
@@ -0,0 +1,10 @@
+
+d1 = raw_input("Dame el archivo: ")
+
+with open(d1,'r') as f:
+    for line in f:
+        for word in line.split():
+           dic = open("dicslipt.txt","a+")
+           dic.write(word + "\n")
+           print word
+    print "split done"   
